663988942d593cc1a4f05feb226822ad5822d458
Charizard17 <3723esad@gmail.com>
2023-06-22 23:00:47 +0200

    initial commit

---
+++ iCalories/iCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Esad Dursun on 22.06.23.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
